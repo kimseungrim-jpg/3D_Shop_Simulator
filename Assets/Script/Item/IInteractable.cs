47838e61f39686af397104bdb1dc8fbd3960b2bd
kimseungrim-jpg <kimseungrim@gmail.com>
2026-07-21 12:37:41 +0900

    Item, UIManager 주석 추가

--- Assets/Script/Item/IInteractable.cs
+++ Assets/Script/Item/IInteractable.cs
@@ -1,7 +1,19 @@
 using UnityEngine;
 
+/// <summary>
+/// 플레이어가 상호작용할 수 있는 오브젝트가 구현해야 하는 곹옽 기능을 정의
+/// </summary>
 public interface IInteractable
 {
+    /// <summary>
+    /// 오브젝트가 담당하는 상호작용 행동을 실행
+    /// 플레이어가 상호작용을 입력 했을 때 상호작용 감지 시스템에서 호출
+    /// </summary>
     void Interact();
+
+    /// <summary>
+    /// 현재 오브젝트와 수행할 수 있는 상호작용 안내 문구를 반환
+    /// 플레이어가 오브젝트를 바라보거나 상호작용 범위에 들어왔을 때 UI 갱신에 사용
+    /// </summary>
     string GetInteractText();
 }
--- Assets/Script/Item/ItemData.cs
+++ Assets/Script/Item/ItemData.cs
@@ -1,5 +1,10 @@
 using UnityEngine;
 
+/// <summary>
+/// 아이템의 이름, 구매, 판매 가격, 월드 생성용 프리팹을 보관하는 데이터 에셋
+/// 여러 아이템 오브젝트와 상점 시스템이 동일한 정보를 공유할 수 있도록
+/// 런타임 동작과 분리된 SO(ScriptableObject)형태로 관리
+/// </summary>
 [CreateAssetMenu(menuName = "Item/ItemData")]
 public class ItemData : ScriptableObject
 {
